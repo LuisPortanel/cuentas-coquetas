31b187822c038428de1d6c0255332da66a381921
Luis Portanel <luis@portanel.com>
2020-03-18 16:30:43 -0600

    CC: Add Google Analytics and update README.md

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Provider } from 'react-redux'
 import { Router } from 'react-router'
 import Routes from './routes'
@@ -8,18 +8,25 @@ import { type Store as ReduxStore, type StoreEnhancer } from 'redux'
 import { type State, type Action } from './redux/reducers'
 import { type BrowserHistory } from 'history'
 
+import ReactGA from 'react-ga'
+ReactGA.initialize('UA-161190257-1')
+
 type Store = ReduxStore<State, StoreEnhancer<State, Action>>
 type AppType = {
   store: Store,
   browserHistory: BrowserHistory
 }
 
-const App = ({ store, browserHistory }: AppType) => (
-  <Provider store={store}>
+const App = ({ store, browserHistory }: AppType) => {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }, [])
+
+  return <Provider store={store}>
     <Router history={browserHistory}>
       <Routes />
     </Router>
   </Provider>
-)
+}
 
 export default App
